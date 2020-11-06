8f2d1513cca2f1f97a63e6f4a374792cdc571a66
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-06 16:42:02 -0500

    commit tree 25b024f3587257436f0811cd04f5f5e79cab3ea5

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+
+class GetRequester
+  attr_reader :url_string
+
+  def initialize(url_string)
+    @url_string = url_string
+  end
+
+  def get_response_body
+    url = @url_string
+    uri = URI.parse(url)
+    response = Net::HTTP.get(uri)
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
